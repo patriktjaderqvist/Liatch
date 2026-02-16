c45108f9a211126c3ec654e6ebc705fd9ee52813
Jesper Malm <jesper.malm@yh.nackademin.se>
2026-02-17 00:05:50 +0100

    Updated settings (secretkey and token_expire)

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Settings(BaseSettings):
     DB_URL: str
-
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    SECRET_KEY: str
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()

449fb135fef0d74842cf4c322c929311a3cd1445
patriktjaderqvist <patrikerikkristoffer.tjaderqvist@yh.nackademin.se>
2026-03-06 22:33:45 +0700

    Same issue persists. Should be fixed now, didnt have react import

--- frontend/src/pages/AnsokningarPage.jsx
+++ frontend/src/pages/AnsokningarPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import { fetchJobAd } from '../lib/jobAdsApi';
 import { fetchJobAdApplications, updateApplicationStatus } from '../lib/applicationsApi';

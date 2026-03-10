5df4e58725a34a37cb031f6865ba9429e2a2c253
Jesper Malm <jesper.malm@yh.nackademin.se>
2026-03-10 17:18:00 +0100

    React import to fix render crash on VaraAnnonser

--- frontend/src/pages/VaraAnnonserPage.jsx
+++ frontend/src/pages/VaraAnnonserPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { deleteJobAd, fetchMyJobAds, updateJobAd } from '../lib/jobAdsApi';
